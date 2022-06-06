399869708439b636dc982ae9b6d6b049bf5b8263
cmaher15 <ceilidhemaher@hotmail.com>
2022-06-06 11:09:14 -0700

    created seeds for tables in DB

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,8 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email) VALUES ('Alice', 'alice@gmail.com');
-INSERT INTO users (name, email) VALUES ('Kira', 'kira@gmail.com');
+INSERT INTO users (name, email, is_admin) VALUES ('Bazil', 'baz@gmail.com', true);
+INSERT INTO users (name, email, is_admin) VALUES ('Ceilidhe', 'ceilidhemaher@gmail.com', true);
+INSERT INTO users (name, email, is_admin) VALUES ('Taylor', 'taylor.mcleod@gmail.com', true);
+INSERT INTO users (name, email, is_admin) VALUES ('Alice', 'alice@gmail.com', false);
+INSERT INTO users (name, email, is_admin) VALUES ('Kira', 'kira@gmail.com', false);
+INSERT INTO users (name, email, is_admin) VALUES ('Micheal', 'mike@gmail.com', false);
+
---
+++ db/seeds/02_items.sql
@@ -0,0 +1,4 @@
+-- Widgets table seeds here (Example)
+INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (1, 'Fox', 'A pretty cool fox costume', '/images/fox', 10);
+INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (2, 'Dog', 'A pretty cool dog costume', '/images/dog', 10);
+INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (3, 'Cat', 'A pretty cool cat costume', '/images/cat', 10);
