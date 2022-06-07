c810d3aa5cfa333e453bc5d13cd78b683fce1879
lorleod <taylor.mcleod@gmail.com>
2022-06-07 23:42:12 +0000

    added item-filter partial for top of item list to filter items. Also started trying to alter db query code and request code to work with filter

--- api_routes/items.js
+++ api_routes/items.js
@@ -8,6 +8,33 @@
 const express = require('express');
 const router  = express.Router();
 
+// getAllItems = (options) => {
+//   const queryParams = [];
+
+//   let queryString = `
+//   SELECT items.*
+//   FROM properties
+//   JOIN favourites ON items.id = favourites.item_id
+//   JOIN users ON users.id = favourites.user_id
+//   `;
+
+//   console.log("queryString: ", queryString);
+//   console.log("queryParams: ", queryParams);
+
+//   db.query(queryString, queryParams)
+//       .then(data => {
+//         const item = data.rows;
+//         console.log("item: ", item)
+//         res.json({ item });
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ error: err.message });
+//       });
+// }
+
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     let query = `SELECT * FROM items`;
