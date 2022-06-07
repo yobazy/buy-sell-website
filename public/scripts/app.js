cf6925fb67e76fe18ded50f7df7a418b66365e74
cmaher15 <ceilidhemaher@hotmail.com>
2022-06-06 22:25:21 -0700

    drafted some possible JQuery functionality, still needs work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,5 +2,31 @@
 
 $
 (()=> {
-  
+  const renderItems = function(items) {
+    for (let item of items) {
+      const $newItem = addNewItem(item);
+      $('.items').append($newItem);
+    }
+  }
+  const addNewItem = function(item) {
+    const $item = (`
+    <div class="layout"><h2>${item.title}</h2>
+    <img src="${item.url}" />
+      <h2>${item.price}</h2>
+      <p>${item.description}</p>
+      <div class="button2">
+      <button class="button">Favourite</button>
+       <button class="button">Buy</button>
+       <button class="button">View</button></div>`)
+
+    return $item;
+  };
+
+const loadItems = function() {
+  $.get('/items', (item) => {
+    renderItems(item);
+  })
+}
+loadItems();
+
 } )
