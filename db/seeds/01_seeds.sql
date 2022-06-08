7453f77d2a0d71837b13058e5a087d99ebc46bae
lorleod <taylor.mcleod@gmail.com>
2022-06-08 23:28:52 +0000

    nav partial to login page

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -9,14 +9,13 @@ INSERT INTO users (name, email, is_admin) VALUES ('Micheal', 'mike@gmail.com', f
 
 -- items table seeds here
 INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (1, 'Fox', 'A pretty cool fox avatar', '/images/foxavatar.jpeg', 10);
-INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (1, 'Anime Girl', 'A pretty cool anime girl avatar', '/images/anigirl.jpg', 11);
-INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (2, 'Dog', 'A pretty cool dog avatar', '/images/dog.jpg', 12);
-INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (2, 'Anime Boy', 'A pretty cool anime boy avatar', '/images/aniboy.jpeg', 11);
-INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (3, 'Cat', 'A pretty cool cat avatar', '/images/cat.gif', 13);
+INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (1, 'Anime Girl', 'A pretty cool anime girl avatar', '/images/anigirl.jpg', 50);
+INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (2, 'Dog', 'A pretty cool dog avatar', '/images/dog.jpg', 35);
+INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (2, 'Anime Boy', 'A pretty cool anime boy avatar', '/images/aniboy.jpeg', 75);
+INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (3, 'Cat', 'A pretty cool cat avatar', '/images/cat.gif', 48);
 INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (3, 'Banana Man', 'A pretty cool banana avatar', '/images/bananaman.webp', 99);
 
 -- favourite table seeds here
-
 INSERT INTO favourites (user_id, item_id) VALUES (1, 1);
 INSERT INTO favourites (user_id, item_id) VALUES (2, 2);
 INSERT INTO favourites (user_id, item_id) VALUES (3, 3);
