2160e495265cb8345f2f238af926094873d26da7
lorleod <taylor.mcleod@gmail.com>
2022-06-09 04:23:00 +0000

    converted prices everywhere to work with price as a x100 integer so that we can do decimal prices... but the sell form isn't accepting decimals for price for some reason. Need to fix that still.

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -8,12 +8,12 @@ INSERT INTO users (name, email, is_admin) VALUES ('Micheal', 'mike@gmail.com', f
 
 
 -- items table seeds here
-INSERT INTO items (user_id, title, description, item_photo_url, price, sold_status) VALUES (1, 'Fox', 'A pretty cool fox avatar', '/images/foxavatar.jpeg', 10, true);
-INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (1, 'Anime Girl', 'A pretty cool anime girl avatar', '/images/anigirl.jpg', 50);
-INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (2, 'Dog', 'A pretty cool dog avatar', '/images/dog.jpg', 35);
-INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (2, 'Anime Boy', 'A pretty cool anime boy avatar', '/images/aniboy.jpeg', 75);
-INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (3, 'Cat', 'A pretty cool cat avatar', '/images/cat.gif', 48);
-INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (3, 'Banana Man', 'A pretty cool banana avatar', '/images/bananaman.webp', 99);
+INSERT INTO items (user_id, title, description, item_photo_url, price, sold_status) VALUES (1, 'Fox', 'A pretty cool fox avatar', '/images/foxavatar.jpeg', 1000, true);
+INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (1, 'Anime Girl', 'A pretty cool anime girl avatar', '/images/anigirl.jpg', 5000);
+INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (2, 'Dog', 'A pretty cool dog avatar', '/images/dog.jpg', 3500);
+INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (2, 'Anime Boy', 'A pretty cool anime boy avatar', '/images/aniboy.jpeg', 7500);
+INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (3, 'Cat', 'A pretty cool cat avatar', '/images/cat.gif', 4800);
+INSERT INTO items (user_id, title, description, item_photo_url, price) VALUES (3, 'Banana Man', 'A pretty cool banana avatar', '/images/bananaman.webp', 9900);
 
 -- favourite table seeds here
 INSERT INTO favourites (user_id, item_id) VALUES (1, 1);
